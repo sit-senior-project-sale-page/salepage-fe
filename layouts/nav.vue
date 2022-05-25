17d9373f518dba1ae214505325936658a8699180
cakeodds <cake@odds.team>
2022-05-25 23:18:51 +0700

    edit nav.vue

--- layouts/nav.vue
+++ layouts/nav.vue
@@ -1,3 +1,17 @@
-<template></template>
+<template>
+  <div>
+    <div class="w-full h-12 nav fixed" style=""></div>
+    <Nuxt />
+  </div>
+</template>
 
-<style></style>
+<script lang="ts">
+import { Vue, Component } from 'nuxt-property-decorator';
+@Component({})
+export default class NavBar extends Vue {}
+</script>
+<style scoped>
+.nav {
+    background-color: rgb(255, 185, 55)
+}
+</style>
